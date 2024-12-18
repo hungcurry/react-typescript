6e7231128e76d733dcd3784b404f04536e74553a
hungcurry <ooopp42@gmail.com>
2024-12-18 13:59:36 +0800

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+/*@type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./index.html", 
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3e0df0',
+        'primary-hover': '#5328ee',
+        'primary-active': '#3a04fa'
+      },
+      container: {
+        center: true,
+        padding: '12px',
+        screens: {
+          DEFAULT: '1024px'
+        }
+      },
+      spacing: {
+        '12': '12px',
+        '40': '40px',
+        '180': '180px'
+      },
+      borderRadius: {
+        '40': '40px'
+      }
+    },
   },
   plugins: [],
 }
